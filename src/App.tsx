6387bc67620e1a4705f602e6c4701563d86b22f7
Lance Bacus <lancebacus55@gmail.com>
2025-05-14 16:15:15 +0800

    Ticket functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,17 @@
+<<<<<<< Updated upstream
 import Box from "@mui/material/Box";
+=======
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate, Link } from 'react-router-dom';
+import './App.css';
+import CreateTicket from './CreateTicket';
+import DeleteTicket from './DeleteTicket';
+import EditTicket from './EditTicket';
+import ViewTicket from './ViewTicket';
+import AssignTicket from './AssignTicket'; // Import your new AssignTicket component
+import ReassignTicket  from './ReAssignTicket';
+import './component-fixes.css';
+>>>>>>> Stashed changes
 
 import { AppRoutes } from "./routes";
 
